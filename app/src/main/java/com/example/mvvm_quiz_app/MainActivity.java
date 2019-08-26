9717629498a6996c5ef74faf0c91c6ae160ba1a2
blckclov3r <blckclov3r@gmail.com>
2019-08-26 06:27:39 -0800

    Preparing the layouts

--- app/src/main/java/com/example/mvvm_quiz_app/MainActivity.java
+++ app/src/main/java/com/example/mvvm_quiz_app/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.mvvm_quiz_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button start_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start_btn = findViewById(R.id.start_button);
 
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,QuizActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
